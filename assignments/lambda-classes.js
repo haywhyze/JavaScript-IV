83ca3282df93a7dc8b8c8717eab290a9653b79b7
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-09 13:06:10 +0100

    created Instructor class with props and methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,6 +8,23 @@ class Person {
   }
 
   speak() {
-    console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+
+class Instructor extends Person {
+  constructor(name, age, location, gender, specialty, favLanguage, catchPhrase) {
+    super(name, age, location, gender);
+    this.specialty = specialty;
+    this.favLanguage = favLanguage;
+    this.catchPhrase = catchPhrase;
+  }
+
+  demo(subject) {
+    return `Today we are learning about ${subject}`
+  }
+
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`
   }
 }
